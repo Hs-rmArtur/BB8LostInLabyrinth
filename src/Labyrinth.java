d6ae21fc0349a3af92b99249bc0a77d39464628e
Michael Fakliier <66269536+MichaelParker007@users.noreply.github.com>
2022-12-19 19:43:15 +0100

    Update Labyrinth.java

--- src/Labyrinth.java
+++ src/Labyrinth.java
@@ -10,17 +10,17 @@ public class Labyrinth {
 		// Deklaration und Instanziierung von Labyrinth
 		int[][] labyrinth = 
 			{
-					{wand,wand,wand,wand,wand,wand,wand,wand,wand,wand,wand,wand}, 	// 0 Zeile
+					{wand,wand,wand,wand,wand,wand,wand,wand,wand,wand,wand,wand}, 		// 0 Zeile
 					{bb8,weg,weg,weg,weg,weg,wand,weg,weg,weg,weg,exit}, 			// 1 Zeile
-					{wand,wand,wand,wand,wand,weg,wand,weg,wand,wand,wand,wand}, 	// 2 Zeile
+					{wand,wand,wand,wand,wand,weg,wand,weg,wand,wand,wand,wand}, 		// 2 Zeile
 					{wand,weg,weg,weg,weg,weg,wand,weg,weg,weg,weg,wand}, 			// 3 Zeile
 					{wand,weg,wand,wand,wand,weg,wand,wand,wand,wand,weg,wand},		// 4 Zeile
 					{wand,weg,wand,weg,weg,weg,wand,weg,weg,weg,weg,wand},			// 5 Zeile
-					{wand,weg,wand,wand,wand,wand,wand,wand,wand,wand,weg,wand},	// 6 Zeile
+					{wand,weg,wand,wand,wand,wand,wand,wand,wand,wand,weg,wand},		// 6 Zeile
 					{wand,weg,weg,weg,weg,weg,weg,wand,weg,wand,weg,wand},			// 7 Zeile
 					{wand,wand,wand,wand,wand,wand,weg,wand,weg,wand,weg,wand},		// 8 Zeile
 					{wand,weg,weg,weg,weg,weg,weg,weg,weg,weg,weg,wand},			// 9 Zeile
-					{wand,wand,wand,wand,wand,wand,wand,wand,wand,wand,wand,wand}	// 10 Zeile
+					{wand,wand,wand,wand,wand,wand,wand,wand,wand,wand,wand,wand}		// 10 Zeile
 			};
 		
 		System.out.println(labyrinth[1][0]);
